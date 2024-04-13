7061fa66ac7a6d75fe3bf5a0201f4c3c329deeb1
Deepak kumar <143509434+Deepak-kumar-2023@users.noreply.github.com>
2024-04-13 21:32:14 +0530

    Add files via upload
    final plant disease detection

---
+++ script.js
@@ -0,0 +1,17 @@
+function searchLetters() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('letterList');
+    li = ul.getElementsByTagName('li');
+  
+    for (i = 0; i < li.length; i++) {
+      a = li[i];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = '';
+      } else {
+        li[i].style.display = 'none';
+      }
+    }
+  }
